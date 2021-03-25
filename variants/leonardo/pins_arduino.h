171163a997f760a47f379d8975879c97973adc89
qitas <qitas@qitas.cn>
2021-03-25 11:28:10 +0800

    fix

--- variants/leonardo/pins_arduino.h
+++ variants/leonardo/pins_arduino.h
@@ -18,8 +18,6 @@
   Public License along with this library; if not, write to the
   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
   Boston, MA  02111-1307  USA
-
-  $Id: wiring.h 249 2007-02-03 16:52:51Z mellis $
 */
 
 #ifndef Pins_Arduino_h
@@ -90,39 +88,63 @@
 #undef OCR2_6
 #undef OCR2_7
 
-#define NUM_DIGITAL_PINS   31
-#define NUM_ANALOG_INPUTS  12
+#define NUM_DIGITAL_PINS  31
+#define NUM_ANALOG_INPUTS 12
 
-#define TXLED0			0
-#define TXLED1			0
-#define RXLED0			0
-#define RXLED1			0
-#define TX_RX_LED_INIT	        0
+#define TX_RX_LED_INIT	DDRD |= (1<<5), DDRB |= (1<<0)
+#define TXLED0			PORTD |= (1<<5)
+#define TXLED1			PORTD &= ~(1<<5)
+#define RXLED0			PORTB |= (1<<0)
+#define RXLED1			PORTB &= ~(1<<0)
 
-static const uint8_t SDA = 2;
-static const uint8_t SCL = 3;
+#define PIN_WIRE_SDA         (2)
+#define PIN_WIRE_SCL         (3)
+
+static const uint8_t SDA = PIN_WIRE_SDA;
+static const uint8_t SCL = PIN_WIRE_SCL;
+
 #define LED_BUILTIN 13
+#define LED_BUILTIN_RX 17
+#define LED_BUILTIN_TX 30
 
 // Map SPI port to 'new' pins D14..D17
-static const uint8_t SS   = 17;
-static const uint8_t MOSI = 16;
-static const uint8_t MISO = 14;
-static const uint8_t SCK  = 15;
+#define PIN_SPI_SS    (17)
+#define PIN_SPI_MOSI  (16)
+#define PIN_SPI_MISO  (14)
+#define PIN_SPI_SCK   (15)
+
+static const uint8_t SS   = PIN_SPI_SS;
+static const uint8_t MOSI = PIN_SPI_MOSI;
+static const uint8_t MISO = PIN_SPI_MISO;
+static const uint8_t SCK  = PIN_SPI_SCK;
 
 // Mapping of analog pins as digital I/O
 // A6-A11 share with digital pins
-static const uint8_t A0 = 18;
-static const uint8_t A1 = 19;
-static const uint8_t A2 = 20;
-static const uint8_t A3 = 21;
-static const uint8_t A4 = 22;
-static const uint8_t A5 = 23;
-static const uint8_t A6 = 24;	// D4
-static const uint8_t A7 = 25;	// D6
-static const uint8_t A8 = 26;	// D8
-static const uint8_t A9 = 27;	// D9
-static const uint8_t A10 = 28;	// D10
-static const uint8_t A11 = 29;	// D12
+#define PIN_A0   (18)
+#define PIN_A1   (19)
+#define PIN_A2   (20)
+#define PIN_A3   (21)
+#define PIN_A4   (22)
+#define PIN_A5   (23)
+#define PIN_A6   (24)
+#define PIN_A7   (25)
+#define PIN_A8   (26)
+#define PIN_A9   (27)
+#define PIN_A10  (28)
+#define PIN_A11  (29)
+
+static const uint8_t A0 = PIN_A0;
+static const uint8_t A1 = PIN_A1;
+static const uint8_t A2 = PIN_A2;
+static const uint8_t A3 = PIN_A3;
+static const uint8_t A4 = PIN_A4;
+static const uint8_t A5 = PIN_A5;
+static const uint8_t A6 = PIN_A6;	// D4
+static const uint8_t A7 = PIN_A7;	// D6
+static const uint8_t A8 = PIN_A8;	// D8
+static const uint8_t A9 = PIN_A9;	// D9
+static const uint8_t A10 = PIN_A10;	// D10
+static const uint8_t A11 = PIN_A11;	// D12
 
 #define digitalPinToPCICR(p)    ((((p) >= 8 && (p) <= 11) || ((p) >= 14 && (p) <= 17) || ((p) >= A8 && (p) <= A10)) ? (&PCICR) : ((uint8_t *)0))
 #define digitalPinToPCICRbit(p) 0
@@ -133,7 +155,7 @@ static const uint8_t A11 = 29;	// D12
 extern const uint8_t PROGMEM analog_pin_to_channel_PGM[];
 #define analogPinToChannel(P)  ( pgm_read_byte( analog_pin_to_channel_PGM + (P) ) )
 
-#define digitalPinHasPWM(p) ((p) == 3 || (p) == 5 || (p) == 6 || (p) == 9 || (p) == 10 || (p) == 11 || (p) == 13)
+#define digitalPinHasPWM(p)         ((p) == 3 || (p) == 5 || (p) == 6 || (p) == 9 || (p) == 10 || (p) == 11 || (p) == 13)
 
 #define digitalPinToInterrupt(p) ((p) == 0 ? 2 : ((p) == 1 ? 3 : ((p) == 2 ? 1 : ((p) == 3 ? 0 : ((p) == 7 ? 4 : NOT_AN_INTERRUPT)))))
 
@@ -143,7 +165,7 @@ extern const uint8_t PROGMEM analog_pin_to_channel_PGM[];
 // for the analog output (software PWM).  Analog input
 // pins are a separate set.
 
-// ATMEL ATMEGA32U4 / ARDUINO LEONARDO / Flora
+// ATMEL ATMEGA32U4 / ARDUINO LEONARDO
 //
 // D0				PD2					RXD1/INT2
 // D1				PD3					TXD1/INT3
@@ -175,8 +197,8 @@ extern const uint8_t PROGMEM analog_pin_to_channel_PGM[];
 // MOSI		D16		PB2					MOSI,PCINT2
 // SS		D17		PB0					RXLED,SS/PCINT0
 //
-// TXLED	D30		PD5
-// RXLED		        PB0
+// TXLED	D30		PD5					XCK1
+// RXLED	D17	    PB0
 // HWB				PE2					HWB
 
 // these arrays map port names (e.g. port B) to the
@@ -221,33 +243,33 @@ const uint8_t PROGMEM digital_pin_to_port_PGM[] = {
 	PC, // D5 - PC6
 	PD, // D6 - PD7
 	PE, // D7 - PE6
-
+	
 	PB, // D8 - PB4
 	PB,	// D9 - PB5
 	PB, // D10 - PB6
 	PB,	// D11 - PB7
 	PD, // D12 - PD6
 	PC, // D13 - PC7
-
+	
 	PB,	// D14 - MISO - PB3
 	PB,	// D15 - SCK - PB1
 	PB,	// D16 - MOSI - PB2
 	PB,	// D17 - SS - PB0
-
+	
 	PF,	// D18 - A0 - PF7
 	PF, // D19 - A1 - PF6
 	PF, // D20 - A2 - PF5
 	PF, // D21 - A3 - PF4
 	PF, // D22 - A4 - PF1
 	PF, // D23 - A5 - PF0
-
+	
 	PD, // D24 / D4 - A6 - PD4
 	PD, // D25 / D6 - A7 - PD7
 	PB, // D26 / D8 - A8 - PB4
 	PB, // D27 / D9 - A9 - PB5
 	PB, // D28 / D10 - A10 - PB6
 	PD, // D29 / D12 - A11 - PD6
-	PD, // D30 - PD5
+	PD, // D30 / TX Led - PD5
 };
 
 const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[] = {
@@ -259,54 +281,54 @@ const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[] = {
 	_BV(6), // D5 - PC6
 	_BV(7), // D6 - PD7
 	_BV(6), // D7 - PE6
-
+	
 	_BV(4), // D8 - PB4
 	_BV(5),	// D9 - PB5
 	_BV(6), // D10 - PB6
 	_BV(7),	// D11 - PB7
 	_BV(6), // D12 - PD6
 	_BV(7), // D13 - PC7
-
+	
 	_BV(3),	// D14 - MISO - PB3
 	_BV(1),	// D15 - SCK - PB1
 	_BV(2),	// D16 - MOSI - PB2
 	_BV(0),	// D17 - SS - PB0
-
+	
 	_BV(7),	// D18 - A0 - PF7
 	_BV(6), // D19 - A1 - PF6
 	_BV(5), // D20 - A2 - PF5
 	_BV(4), // D21 - A3 - PF4
 	_BV(1), // D22 - A4 - PF1
 	_BV(0), // D23 - A5 - PF0
-
+	
 	_BV(4), // D24 / D4 - A6 - PD4
 	_BV(7), // D25 / D6 - A7 - PD7
 	_BV(4), // D26 / D8 - A8 - PB4
 	_BV(5), // D27 / D9 - A9 - PB5
 	_BV(6), // D28 / D10 - A10 - PB6
 	_BV(6), // D29 / D12 - A11 - PD6
-	_BV(5), // D30 - PD5
+	_BV(5), // D30 / TX Led - PD5
 };
 
 const uint8_t PROGMEM digital_pin_to_timer_PGM[] = {
-	NOT_ON_TIMER,
+	NOT_ON_TIMER,	
 	NOT_ON_TIMER,
 	NOT_ON_TIMER,
 	TIMER0B,		/* 3 */
 	NOT_ON_TIMER,
 	TIMER3A,		/* 5 */
 	TIMER4D,		/* 6 */
-	NOT_ON_TIMER,
-
-	NOT_ON_TIMER,
+	NOT_ON_TIMER,	
+	
+	NOT_ON_TIMER,	
 	TIMER1A,		/* 9 */
 	TIMER1B,		/* 10 */
 	TIMER0A,		/* 11 */
-
-	NOT_ON_TIMER,
+	
+	NOT_ON_TIMER,	
 	TIMER4A,		/* 13 */
-
-	NOT_ON_TIMER,
+	
+	NOT_ON_TIMER,	
 	NOT_ON_TIMER,
 	NOT_ON_TIMER,
 	NOT_ON_TIMER,
@@ -328,11 +350,11 @@ const uint8_t PROGMEM digital_pin_to_timer_PGM[] = {
 
 const uint8_t PROGMEM analog_pin_to_channel_PGM[] = {
 	7,	// A0				PF7					ADC7
-	6,	// A1				PF6					ADC6
-	5,	// A2				PF5					ADC5
+	6,	// A1				PF6					ADC6	
+	5,	// A2				PF5					ADC5	
 	4,	// A3				PF4					ADC4
-	1,	// A4				PF1					ADC1
-	0,	// A5				PF0					ADC0
+	1,	// A4				PF1					ADC1	
+	0,	// A5				PF0					ADC0	
 	8,	// A6		D4		PD4					ADC8
 	10,	// A7		D6		PD7					ADC10
 	11,	// A8		D8		PB4					ADC11
@@ -365,23 +387,5 @@ const uint8_t PROGMEM analog_pin_to_channel_PGM[] = {
 
 // Alias SerialUSB to Serial
 #define SerialUSB SERIAL_PORT_USBVIRTUAL
-
-// Bootloader related fields
-// Old Caterina bootloader places the MAGIC key into unsafe RAM locations (it can be rewritten
-// by the running sketch before to actual reboot).
-// Newer bootloaders, recognizable by the LUFA "signature" at the end of the flash, can handle both
-// the usafe and the safe location. Check once (in USBCore.cpp) if the bootloader in new, then set the global
-// _updatedLUFAbootloader variable to true/false and place the magic key consequently
-#ifndef MAGIC_KEY
-#define MAGIC_KEY 0x7777
-#endif
-
-#ifndef MAGIC_KEY_POS
-#define MAGIC_KEY_POS 0x0800
-#endif
-
-#ifndef NEW_LUFA_SIGNATURE
-#define NEW_LUFA_SIGNATURE 0xDCFB
-#endif
 
 #endif /* Pins_Arduino_h */
